1b6ff55b6e698d048baa374223257d01926d9eeb
neerajk2 <60866470+neerajk2@users.noreply.github.com>
2020-03-05 11:09:43 +0530

    Create LogEntry.java

---
+++ WebLogs/LogEntry.java
@@ -0,0 +1,43 @@
+
+/**
+ * Declares a class called LogEntry to keep apache web log records.
+ * 
+ * @author (Neeraj Kankani)
+ */
+
+import  java.util.*;
+public class LogEntry
+{
+     private String ipAddress;
+     private Date accessTime;
+     private String request;
+     private int statusCode;
+     private int bytesReturned;
+     
+   public LogEntry(String ip, Date time, String req, int status, int bytes) {
+       ipAddress = ip;
+       accessTime = time;
+       request = req;
+       statusCode = status;
+       bytesReturned = bytes;
+   }
+   
+   public String getIpAddress() {
+         return ipAddress;
+    }
+    public Date getAccessTime() { return accessTime; }
+   public String getRequest() {
+         return request;
+   }
+   public int getStatusCode() {
+         return statusCode;
+   }
+   public int getBytesReturned() {
+         return bytesReturned;
+   }
+   
+   public String toString() {
+       return ipAddress + " " + accessTime + " " + request 
+           + " " + statusCode + " " + bytesReturned;
+    }
+}
